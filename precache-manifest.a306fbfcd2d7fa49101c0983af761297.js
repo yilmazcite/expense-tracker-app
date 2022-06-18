34635008a5624e004b255b4d1e637c7e9d80617b
Yilmaz Cite <yilmazcite@gmail.com>
2022-06-18 18:30:36 +0300

    Updates

---
+++ precache-manifest.a306fbfcd2d7fa49101c0983af761297.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9745fa0a7d886b3bd5ca",
+    "url": "/expense-tracker-app/static/css/main.8be15b43.chunk.css"
+  },
+  {
+    "revision": "9745fa0a7d886b3bd5ca",
+    "url": "/expense-tracker-app/static/js/main.9745fa0a.chunk.js"
+  },
+  {
+    "revision": "ef722a117889fe2e78cc",
+    "url": "/expense-tracker-app/static/js/1.ef722a11.chunk.js"
+  },
+  {
+    "revision": "9bccffbe386088b22d60",
+    "url": "/expense-tracker-app/static/js/runtime~main.9bccffbe.js"
+  },
+  {
+    "revision": "77e9ef67cfc83065c424140f411c37aa",
+    "url": "/expense-tracker-app/index.html"
+  }
+];
